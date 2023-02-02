bbce95da7ba2c4ec4d4d5c61f5aa24269432302c
galexandrus <gavrilchuk_a@mail.ru>
2023-02-02 19:13:39 +0300

    topic 3, lab task 1 (according to the task)

--- Занятие 3. Инкапсуляция, наследование, полиморфизм/2. Лабораторные задания/task1/main.py
+++ Занятие 3. Инкапсуляция, наследование, полиморфизм/2. Лабораторные задания/task1/main.py
@@ -1,31 +1,41 @@
+from typing import Union
+
+
 class Book:
     """ Базовый класс книги. """
     def __init__(self, name: str, author: str):
-        self.name = name
-        self.author = author
+        self.__name = name
+        self.__author = author
 
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        return f"Книга {self.__name}. Автор {self.__author}"
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+        return f"{self.__class__.__name__}(name={self.__name!r}, author={self.__author!r})"
+
+    @staticmethod
+    def check_value(value: Union[int, float]):
+        if value <= 0:
+            raise ValueError
+        elif value is not Union[int, float]:
+            raise TypeError
 
 
-class PaperBook:
+class PaperBook(Book):
     def __init__(self, name: str, author: str, pages: int):
-        self.name = name
-        self.author = author
+        super().check_value(pages)
+        super().__init__(name, author)
         self.pages = pages
 
-    def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.__name!r}, author={self.__author!r}, pages={self.pages!r})"
 
 
-class AudioBook:
+class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
-        self.name = name
-        self.author = author
+        super().check_value(duration)
+        super().__init__(name, author)
         self.duration = duration
 
-    def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.__name!r}, author={self.__author!r}, duration={self.duration!r})"
