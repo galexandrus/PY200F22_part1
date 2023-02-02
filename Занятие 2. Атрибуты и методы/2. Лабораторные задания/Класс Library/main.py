466c83cb4b14905279a7e4096a72e109e59d4bde
anjolras <anjolras@yandex.ru>
2023-02-02 16:23:45 +0300

    topic 2 lab task 2 Library with method description

--- Занятие 2. Атрибуты и методы/2. Лабораторные задания/Класс Library/main.py
+++ Занятие 2. Атрибуты и методы/2. Лабораторные задания/Класс Library/main.py
@@ -43,16 +43,30 @@ class Book:
         """
         return f"{self.__class__.__name__}(id_={self.id_}, name='{self.name}', pages={self.pages})"
 
+
 # DONE написать класс Library
 class Library:
 
-    def __init__(self, books: Optional[Book] = None):
-        if books is not None:
-            self.books = books
-        else:
+    def __init__(self, books: Optional[list] = None):
+        """
+        Конструктор библиотеки
+        :param books: список книг
+        """
+        if books is None:
             self.books = []
+        else:
+            if not isinstance(books, list):
+                raise TypeError("необходимо передать список книг")
+            for book in books:
+                if not isinstance(book, Book):
+                    raise TypeError("список должен состоять из книг")
+            self.books = books
 
-    def get_next_book_id(self):
+    def get_next_book_id(self) -> int:
+        """
+        Метод для получения идентификатора для добавления новой книги в библиотеку
+        :return: идентификатор (атрибут экземпляра класса Book)
+        """
         if len(self.books) == 0:
             return 1
         else:
@@ -60,13 +74,16 @@ class Library:
             return last_book.id_ + 1
 
     def get_index_by_book_id(self, id_: Optional[int] = None):
-        index = 0
-        for book in self.books:
+        """
+        Метод, возвращающий индекс книги в списке
+        :param id_: id книги, индекс которой нужно найти
+        :return: индекс (порядковый номер в списке книг)
+        """
+        for i in range(len(self.books)):
+            book = self.books[i]
             if book.id_ == id_:
-                return index
-            index += 1
-        else:
-            raise ValueError
+                return i
+        raise ValueError("Книги с запрашиваемым id не существует")
 
 
 if __name__ == '__main__':
@@ -74,9 +91,20 @@ if __name__ == '__main__':
     print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
 
     list_books = [
-        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+        Book(id_=book_dict["id"],
+             name=book_dict["name"],
+             pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
     ]
+
     library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
     print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
 
     print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
+    # print(library_with_books.get_index_by_book_id(2))
+
+    # print(isinstance(list_books, list))
+    # for book in list_books:
+    #     print(isinstance(book, Book))
+    #     print(f"{book.__class__.__name__}(id_={book.id_}, name='{book.name}', pages={book.pages})")
+    # print(type(list_books))
+    # print(type(list()))
